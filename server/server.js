44172f86884b5ccb1e1169365481ae0408b035e1
Abdullah Aleem <aaleem.bscs21seecs@seecs.edu.pk>
2024-08-10 18:04:20 +0500

    ConnectionToMongo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+const dbURI = process.env.MONGO_URL; // Replace with your MongoDB connection string
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 
 app.get("/", (req, res) => {
